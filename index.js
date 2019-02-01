9a2e7a3f4dd25a9f4ed1566f55110735abcc16bb
Jonathan Heinz <jonathan.heinz@aol.com>
2019-02-01 09:31:24 -0800

    stretch tests pass

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ const server = express();
 
 server.use(express.json());
 
-const games = [];
+const games = [{
+  title: 'centipede',
+  genre: 'arcade',
+  id: 1
+}];
+
+let id = 2;
 
 server.get('/games', (req, res) => {
 
   res.status(200).json(games);
+
+});
+
+server.get('/games/:id', (req, res) => {
+
+  const gameID = req.params.id;
+
+  const exists = games.find(game => gameID == games.id);
+
+  if (exists) {
+
+    res.status(200).json(exists);
+
+  }
+
+  else {
+
+    res.status(404).json({message: 'not found'});
+
+  }
 
 });
 
@@ -23,9 +49,44 @@ server.post('/games', (req, res) => {
 
   }
 
-  games.push({ title, genre, releaseYear });
+  const found = games.find(game => game.title === title);
+
+  if (found) {
+
+    res.status(405).json({message: 'duplicate name'});
+    return;
+
+  }
+
+  games.push({ id, title, genre, releaseYear });
+
+  id++;
 
   res.status(201).json({message: 'success'});
+
+});
+
+server.delete('/games/:id', (req, res) => {
+
+  const gameID = req.params.id;
+
+  const game = games.find(game => game.id == gameID);
+
+  console.log(gameID, game);
+
+  if (game === undefined) {
+
+    res.status(404).json({message: 'not found'});
+
+  }
+
+  else {
+
+    const index = games.indexOf(game);
+    games.splice(index, 0);
+    res.status(200).json({message: 'success'});
+
+  }
 
 });
 
