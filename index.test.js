72161af556f34fc4618d7ed07a45341d3ee7d2b5
Jonathan Heinz <jonathan.heinz@aol.com>
2019-02-01 09:13:09 -0800

    wrote tests for stretch

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const server = require('./index');
 
 describe('index.js', () => {
 
-  describe('GET routes', () => {
+  describe('GET /games', () => {
 
     it('should return status code 200', async () => {
 
@@ -31,7 +31,27 @@ describe('index.js', () => {
 
   });
 
-  describe('POST routes', () => {
+  describe('GET /games/:id', () => {
+
+    it('should return a single game', async () => {
+
+      const response = await request(server).get('/games/1');
+
+      expect(response.body.title).not.toBe(null);
+
+    });
+
+    it('should 404 if game does not exist', async () => {
+
+      const response = await request(server).get('/games/123');
+
+      expect(response.status).toBe(404);
+
+    });
+
+  });
+
+  describe('POST /games', () => {
 
     it('should return 201 status code if body is valid', async () => {
 
@@ -49,11 +69,40 @@ describe('index.js', () => {
 
     });
 
+    it('should return 405 status code if duplicate names are entered', async () => {
+
+      await request(server).post('/games').send({title: 'Tetris', genre: 'Arcade'});
+      const response = await request(server).post('/games').send({title: 'Tetris', genre: 'Arcade'});
+
+      expect(response.status).toBe(405);
+
+    });
+
     it('should return a message in body', async () => {
 
       const response = await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade' });
 
       expect(response.body.message).not.toBe(null);
+
+    });
+
+  });
+
+  describe('DELETE /games/:id', () => {
+
+    it('should return 200 if game exists', async () => {
+
+      const response = await request(server).delete('/games/1');
+
+      expect(response.status).toBe(200);
+
+    });
+
+    it('should 404 if game does not exist', async () => {
+
+      const response = await request(server).delete('/games/123');
+
+      expect(response.status).toBe(404);
 
     });
 
